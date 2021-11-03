9028104519bc0b2c852dc22ed4c5313feb49dbe7
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-02 21:59:11 -0700

    add query 2

--- world_queries.sql
+++ world_queries.sql
@@ -3,11 +3,20 @@
 -- By:      Virgilio D. Cabading Jr.    Created: November 02, 2021
 -- ////////////////////////////////////////////////////////
 
--- get all the countries that speak Slovene 
---      Return the name of the country, language and language percentage
---      Arrange the result by language percentage in descending order
+-- 1.   get all the countries that speak Slovene 
+--          Return the name of the country, language and language percentage
+--          Arrange the result by language percentage in descending order
 
 SELECT countries.name, languages.language, languages.percentage FROM countries
 JOIN languages ON countries.id = languages.country_id
 WHERE languages.language = 'Slovene'
 ORDER BY languages.percentage DESC;
+
+-- 2.   display the total number of cities for each country
+--          return the name of the country and the total number of cities
+--          arrange the result by the number of cities in descending order
+
+SELECT countries.name, COUNT(*) FROM countries
+JOIN cities ON countries.id = cities.country_id
+GROUP BY countries.id
+ORDER BY COUNT(*) DESC;
